f9b29cb842831660e1554772e60a3ba92617c219
Artem Koziar <koziar.artem@gmail.com>
2023-02-09 14:00:07 +0200

    add prettier and sort keys

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # eslint rules for TypeScript React applications
 
 ```
-yarn add -D eslint typescript tsc prettier eslint-config-next eslint-config-react-app eslint-import-resolver-typescript eslint-loader eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-sonarjs
+yarn add -D eslint typescript tsc prettier eslint-config-next eslint-config-react-app eslint-import-resolver-typescript eslint-loader eslint-plugin-flowtype eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-sonarjs eslint-plugin-sort-keys-fix
 ```
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+
+const x = {
+  a: 1,
+  b: 2,
+  d: 'd'
+};
 
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img alt="logo" className="App-logo" src={logo} />
+        <h1>{x.a}</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
+        <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
           Learn React
         </a>
       </header>
